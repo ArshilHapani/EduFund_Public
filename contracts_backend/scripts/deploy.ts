6a92bd568e3d50c79317720f4402903b8b1f730f
ArshilHapani <arshilhapani998@gmail.com>
2024-08-07 22:59:57 +0530

    Counter contract initialized

--- contracts_backend/scripts/deploy.ts
+++ contracts_backend/scripts/deploy.ts
@@ -6,4 +6,13 @@ import { ethers } from "hardhat";
   await counter.waitForDeployment();
 
   console.log("Counter deployed to:", await counter.getAddress());
+  let counterValue = await counter.count();
+  console.log(`counter value ${counterValue}`);
+  // increment
+  await counter.increment();
+  counterValue = await counter.count();
+  console.log(`counter value after increment ${counterValue}`);
+  await counter.decrement();
+  counterValue = await counter.count();
+  console.log(`counter value after decrement ${counterValue}`);
 })();
