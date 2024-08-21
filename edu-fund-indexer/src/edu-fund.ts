47528929e5731374453236fda9fd6b935763a3bc
ArshilHapani <arshilhapani998@gmail.com>
2024-08-21 14:06:33 +0530

    indexer added and typography plugin configured

--- edu-fund-indexer/src/edu-fund.ts
+++ edu-fund-indexer/src/edu-fund.ts
@@ -4,33 +4,33 @@ import {
   CampaignVoted as CampaignVotedEvent,
   DonationReceived as DonationReceivedEvent,
   TransactionExecuted as TransactionExecutedEvent,
-  TransactionProposed as TransactionProposedEvent
-} from "../generated/EduFund/EduFund"
+  TransactionProposed as TransactionProposedEvent,
+} from "../generated/EduFund/EduFund";
 import {
   CampaignCreated,
   CampaignMadeInactive,
   CampaignVoted,
   DonationReceived,
   TransactionExecuted,
-  TransactionProposed
-} from "../generated/schema"
+  TransactionProposed,
+} from "../generated/schema";
 
 export function handleCampaignCreated(event: CampaignCreatedEvent): void {
   let entity = new CampaignCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.campaignId = event.params.campaignId
-  entity.title = event.params.title
-  entity.description = event.params.description
-  entity.goal = event.params.goal
-  entity.deadline = event.params.deadline
+  );
+  entity.owner = event.params.owner;
+  entity.campaignId = event.params.campaignId;
+  entity.title = event.params.title;
+  entity.description = event.params.description;
+  entity.goal = event.params.goal;
+  entity.deadline = event.params.deadline;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCampaignMadeInactive(
@@ -38,45 +38,45 @@ export function handleCampaignMadeInactive(
 ): void {
   let entity = new CampaignMadeInactive(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignId = event.params.campaignId
+  );
+  entity.campaignId = event.params.campaignId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCampaignVoted(event: CampaignVotedEvent): void {
   let entity = new CampaignVoted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.voter = event.params.voter
-  entity.campaignId = event.params.campaignId
-  entity.transactionIndex = event.params.transactionIndex
-  entity.vote = event.params.vote
+  );
+  entity.voter = event.params.voter;
+  entity.campaignId = event.params.campaignId;
+  entity.transactionIndex = event.params.transactionIndex;
+  entity.vote = event.params.vote;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDonationReceived(event: DonationReceivedEvent): void {
   let entity = new DonationReceived(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.donor = event.params.donor
-  entity.amount = event.params.amount
-  entity.campaignId = event.params.campaignId
+  );
+  entity.donor = event.params.donor;
+  entity.amount = event.params.amount;
+  entity.campaignId = event.params.campaignId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransactionExecuted(
@@ -84,15 +84,15 @@ export function handleTransactionExecuted(
 ): void {
   let entity = new TransactionExecuted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignId = event.params.campaignId
-  entity.transactionIndex = event.params.transactionIndex
+  );
+  entity.campaignId = event.params.campaignId;
+  entity.transactionIndex = event.params.transactionIndex;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransactionProposed(
@@ -100,16 +100,16 @@ export function handleTransactionProposed(
 ): void {
   let entity = new TransactionProposed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.campaignId = event.params.campaignId
-  entity.recipients = event.params.recipients
-  entity.amounts = event.params.amounts
-  entity.descriptions = event.params.descriptions
+  );
+  entity.owner = event.params.owner;
+  entity.campaignId = event.params.campaignId;
+  entity.recipients = event.params.recipients;
+  entity.amounts = event.params.amounts;
+  entity.descriptions = event.params.descriptions;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
